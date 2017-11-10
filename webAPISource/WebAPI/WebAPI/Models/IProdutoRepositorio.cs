c41804ace6beda604d329e27ae6ee39461f7e5a8
LUIZ CARLOS DE Junior <luizjralves@gmail.com>
2017-11-10 16:37:01 -0200

    inicio

---
+++ webAPISource/WebAPI/WebAPI/Models/IProdutoRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public interface IProdutoRepositorio
+{
+    IEnumerable<Produto> GetAll();
+    Produto Get(int id);
+    Produto Add(Produto item);
+    void Remove(int id);
+    bool Update(Produto item);
+}
